18edacbab5a597acd2dfd9c2a0170dbc65f92f46
human2-0 <matthew.dworek@hotmail.com>
2022-06-26 14:54:25 +0100

    make_url fix, didn't redirect to target url

--- main.py
+++ main.py
@@ -1,53 +1,11 @@
-from fastapi import FastAPI, HTTPException, Depends, Response, status
-from fastapi.responses import RedirectResponse
-from sqlalchemy.orm import Session
-from database import SessionLocal, engine
-import crud
-import models
-import schemas
-import utils
+import uvicorn
+from fastapi import FastAPI
 
-
-models.Base.metadata.create_all(bind=engine)
+from fastapilush.urlshortener.routers.urlshortener_routes import router
 
 app = FastAPI()
 
+app.include_router(router, prefix='', tags=['urlshortener'])
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-@app.get('/')
-def index():
-    return {"url": "shortener"}
-
-
-@app.post('/url/', response_model=schemas.UrlOut)
-def make_url(url: schemas.UrlIn, response: Response, db: Session = Depends(get_db)):
-    db_url = crud.get_url(db, longurl=url.longurl)
-
-    if db_url:
-        raise HTTPException(
-            status_code=200,
-            detail="The url already registered. Previously created short url: " + db_url.shorturl
-        )
-    else:
-        response.status_code = status.HTTP_201_CREATED
-
-    schemas.UrlOut.shorturl = utils.create_shorturl()
-    output = {'longurl': url.longurl, 'shorturl': schemas.UrlOut.shorturl}
-    targeturl = crud.create_url(db=db, url=output)
-    return targeturl
-
-
-@app.get("/{shorturl}")
-def shorturlflow(shorturl: str, db: Session = Depends(get_db)):
-    flow = crud.get_shorturlflow(db=db, shorturl=shorturl)
-    if flow is not None:
-        return RedirectResponse(flow.longurl, status_code=status.HTTP_301_MOVED_PERMANENTLY)
-    else:
-        raise HTTPException(status_code=400, detail="The path does not exist.")
+if __name__ == "__main__":
+    uvicorn.run(app='main:app')
